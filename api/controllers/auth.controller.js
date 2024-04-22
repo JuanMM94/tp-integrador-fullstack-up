ac2d06eef94f16f15b0d6f71a2f6c79b7794ef5b
Juan Martín Monasterio <juanmartinmonasterio@gmail.com>
2024-04-22 11:34:31 -0300

    implement jwt & plushies endpoints

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
-const bcrypt = require("bcryptjs");
+require("dotenv").config();
+
 const User = require("../models/user.model");
+const { JWT_TIMEOUT } = require("./utils/constants");
 const { getToken, createToken } = require("./utils/jwt");
 
 exports.signup_post = async (req, res) => {
@@ -13,7 +15,10 @@ exports.signup_post = async (req, res) => {
     const user = new User({ email, password, name, roles });
     await user.save();
 
-    return res.status(201).json(user);
+    const token = createToken(user._id);
+
+    res.cookie("jwt", token, { httpOnly: true, maxAge: JWT_TIMEOUT * 1000 });
+    return res.status(201).json({ user: user._id });
   } catch (error) {
     console.error(error);
     return res.status(400).json(error);
@@ -22,31 +27,31 @@ exports.signup_post = async (req, res) => {
 
 exports.login_get = async (req, res) => {
   try {
-    const token = req.userId;
+    const userId = req.userId;
 
-    return res.status(200).json({ token });
+    return res.status(200).json({ userId });
   } catch (error) {
     return res.status(400).json(error);
   }
 };
 
 exports.login_post = async (req, res) => {
+  const { email, password } = req.body;
+
   try {
-    const { email, password } = req.body;
-
-    const user = await User.findOne({ email });
-
-    const isMatch = await user.comparePassword(password);
-
-    if (!isMatch) {
-      return res.status(401).json({ error: "Authentication failed" });
-    }
+    const user = await User.login(email, password);
 
     const token = createToken(user._id);
 
-    return res.status(200).json({ token });
+    res.cookie("jwt", token, { httpOnly: true, maxAge: JWT_TIMEOUT * 1000 });
+    return res.status(200).json({ user: user._id });
   } catch (error) {
-    console.log(error);
-    return res.status(500).send(error);
+    console.error(error);
+    return res.status(400).json({ error: error.message });
   }
+};
+
+exports.logout_get = (req, res) => {
+  res.clearCookie("jwt");
+  return res.sendStatus(204);
 };
--- api/controllers/plush.controller.js
+++ api/controllers/plush.controller.js
@@ -1,11 +1,33 @@
-const Plush = require("../models/plush.model");
+const Plushie = require("../models/plushie.model");
+const User = require("../models/user.model");
 
-exports.getAllPlushes = async (req, res) => {
+exports.plushies_get = async (req, res) => {
   try {
-    const plushes = await Plush.find({});
+    const plushies = await Plushie.find({});
 
-    res.json(plushes);
+    return res.status(200).json(plushies);
   } catch (error) {
-    console.status(500).json({ message: error.message });
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+exports.plushies_post = async (req, res) => {
+  const { type, color, props } = req.body;
+  const id = req.userId.data;
+
+  try {
+    const plushie = new Plushie({ _creator: id, type, color, props });
+
+    await plushie.save();
+
+    const user = await User.findOne({ _id: id }).populate("plushies").exec();
+
+    user.plushies.push(plushie);
+
+    await user.save();
+
+    return res.status(201).json(plushie);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
   }
 };
