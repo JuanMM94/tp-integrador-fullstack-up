3210d847221f93a8d95fe6aede71d2cab04c4246
Juan Martín Monasterio <juanmartinmonasterio@gmail.com>
2024-04-22 16:42:27 -0300

    refactor auth endpoints

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth.controller");
-const authMiddleware = require("../middleware/auth.middleware");
 
 router.post("/auth/signup", authController.signup_post);
-router.get("/auth/login", authMiddleware.verifyToken, authController.login_get);
 router.post("/auth/login", authController.login_post);
+router.get("/auth/logout", authController.logout_get)
 
 module.exports = router;
