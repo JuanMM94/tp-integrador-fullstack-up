f19a909d20bf3c93680f82e04f8a8e16a939ce30
Juan Martín Monasterio <juanmartinmonasterio@gmail.com>
2024-04-27 03:44:01 -0300

    add confirmation_get route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 
 const User = require("../models/user.model");
 const { JWT_TIMEOUT } = require("../utils/constants");
-const { createToken } = require("./utils/jwt");
+const { sendEmail } = require("./utils/emailSender");
+const { createToken, verifyToken } = require("./utils/jwt");
 
 exports.signup_post = async (req, res) => {
   try {
@@ -11,9 +12,18 @@ exports.signup_post = async (req, res) => {
     const user = new User({ email, password, name });
     await user.save();
 
-    const token = createToken(user._id);
+    const confirmToken = createToken(user.email);
+    const loginToken = createToken(user._id);
 
-    res.cookie("jwt", token, { httpOnly: true, maxAge: JWT_TIMEOUT * 1000 });
+    user.token = confirmToken;
+    await user.save();
+
+    await sendEmail(email, confirmToken);
+
+    res.cookie("jwt", loginToken, {
+      httpOnly: true,
+      maxAge: JWT_TIMEOUT * 1000,
+    });
     return res.status(201).json({ user: user._id });
   } catch (error) {
     console.error(error);
@@ -43,5 +53,30 @@ exports.logout_get = (req, res) => {
     return res.sendStatus(204);
   } catch (error) {
     return res.sendStatus(403);
+  }
+};
+
+exports.confirmation_get = async (req, res) => {
+  const { token } = req.params;
+  req.assert
+
+  try {
+    const user = await User.findOne({ token });
+
+    if (!user) return res.status(404).send({ error: "Not found" });
+
+    verifyToken(token, async (error, decoded) => {
+      if (error) {
+        return res.status(403).json({ error: error.message });
+      }
+
+      user.isConfirmed = true;
+      await user.save();
+    });
+
+    return res.sendStatus(204);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: error.message });
   }
 };
