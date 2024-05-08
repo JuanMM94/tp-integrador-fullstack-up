94d5f4a508a953c5f411d0cd839b0be133cf865c
Juan Martín Monasterio <juanmartinmonasterio@gmail.com>
2024-05-07 23:56:52 -0300

    fix delete associated tokens to user

--- api/models/token.model.js
+++ api/models/token.model.js
@@ -13,9 +13,19 @@ const TokenSchema = new Schema({
   expireAt: {
     type: Date,
     required: true,
-    default: new Date(Date.now() + JWT_TIMEOUT * 1000 ),
+    default: new Date(Date.now() + JWT_TIMEOUT * 1000),
     expires: 60,
   },
+});
+
+TokenSchema.pre("save", async function (next) {
+  try {
+    await Token.deleteMany({ _userId: this._userId });
+    next();
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 });
 
 const Token = mongoose.model("Token", TokenSchema);
